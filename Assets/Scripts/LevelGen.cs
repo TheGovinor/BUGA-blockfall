194a24198e84e5c37c5f4eb5030e5694d8131a01
thegovinor <kjgzone@gmail.com>
2016-09-27 18:42:43 -0500

    Initial Commit

---
+++ Assets/Scripts/LevelGen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGen : MonoBehaviour {
+	GameObject square;
+	public int dimensionX;
+	public int dimensionY;
+	public GameObject[][] levelAry;
+
+	// Use this for initialization
+	void Start () {
+		square = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		for (int i = 0; i < dimensionX; i++) {
+			//for (int j = 0; 
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+					
+	
+	}
+}
