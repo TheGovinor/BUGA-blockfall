36355615d324e8b3e17684824a522932bf58e35a
twaster56123 <twaster56123@gmail.com>
2016-09-27 20:17:34 -0500

    Eric's Push

--- Assets/Scripts/LevelGen.cs
+++ Assets/Scripts/LevelGen.cs
@@ -2,31 +2,23 @@
 using System.Collections;
 
 public class LevelGen : MonoBehaviour {
-	public int dimensionX = 5;
-	public int dimensionY = 5;
-	Transform levelParent;
+	GameObject square;
+	public int dimensionX;
+	public int dimensionY;
+	public GameObject[][] levelAry;
 
 	// Use this for initialization
 	void Start () {
-		GameObject[,] levelAry = new GameObject[dimensionX, dimensionY];
-		//square = GameObject.CreatePrimitive (PrimitiveType.Cube);
-		levelParent = GameObject.Find ("Level").GetComponent<Transform> ().transform;
-		//square.transform.SetParent (levelParent);
-
-
-		//create Level based on 2D array, instantiate cubes at each value
-		for (int x = 0; x < dimensionX; x++) {
-			for (int y = 0; y < dimensionY; y++) {
-				GameObject temp = Instantiate (Resources.Load("Prefabs/Cube"), new Vector3 (x, 0, y), Quaternion.identity, levelParent) as GameObject;
-			}
+		square = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		for (int i = 0; i < dimensionX; i++) {
+			//for (int j = 0; 
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-
-
+					
 	
 	}
+
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+	int lives = 3;
+	bool[] keys = new bool[7000];
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		//Horizontal keys (a,d)
+		if(Input.GetKey("fire1")){
+
+		}
+
+		//if(Input.GetAxis()){
+
+
+
+	}
+}
+
