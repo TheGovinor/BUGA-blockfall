99d25024b9067f72282934fdeec9bfce4843c293
thegovinor <kjgzone@gmail.com>
2016-09-27 19:43:08 -0500

    Sept 27, 2016 Commit

--- Assets/Scripts/LevelGen.cs
+++ Assets/Scripts/LevelGen.cs
@@ -2,22 +2,31 @@
 using System.Collections;
 
 public class LevelGen : MonoBehaviour {
-	GameObject square;
-	public int dimensionX;
-	public int dimensionY;
-	public GameObject[][] levelAry;
+	public int dimensionX = 5;
+	public int dimensionY = 5;
+	Transform levelParent;
 
 	// Use this for initialization
 	void Start () {
-		square = GameObject.CreatePrimitive (PrimitiveType.Cube);
-		for (int i = 0; i < dimensionX; i++) {
-			//for (int j = 0; 
+		GameObject[,] levelAry = new GameObject[dimensionX, dimensionY];
+		//square = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		levelParent = GameObject.Find ("Level").GetComponent<Transform> ().transform;
+		//square.transform.SetParent (levelParent);
+
+
+		//create Level based on 2D array, instantiate cubes at each value
+		for (int x = 0; x < dimensionX; x++) {
+			for (int y = 0; y < dimensionY; y++) {
+				GameObject temp = Instantiate (Resources.Load("Prefabs/Cube"), new Vector3 (x, 0, y), Quaternion.identity, levelParent) as GameObject;
+			}
 		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-					
+		
+
+
 	
 	}
 }
