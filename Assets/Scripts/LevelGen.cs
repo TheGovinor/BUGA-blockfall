eecaaf31e82e66381f9cb94abbb6e8d055026111
thegovinor <kjgzone@gmail.com>
2016-10-11 18:35:32 -0500

    Oct 11,2016

--- Assets/Scripts/LevelGen.cs
+++ Assets/Scripts/LevelGen.cs
@@ -2,16 +2,18 @@
 using System.Collections;
 
 public class LevelGen : MonoBehaviour {
-	public int dimensionX = 5;
-	public int dimensionY = 5;
+	//this script generates a level for the game, dimensions are public and can be set outside the class
+	//level is created into squares and each is added to a 2d array
+
+
+	public int dimensionX = 20;
+	public int dimensionY = 20;
 	Transform levelParent;
 
 	// Use this for initialization
 	void Start () {
 		GameObject[,] levelAry = new GameObject[dimensionX, dimensionY];
-		//square = GameObject.CreatePrimitive (PrimitiveType.Cube);
 		levelParent = GameObject.Find ("Level").GetComponent<Transform> ().transform;
-		//square.transform.SetParent (levelParent);
 
 
 		//create Level based on 2D array, instantiate cubes at each value
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+/*
+	This Script is designed to control movement and holds values such as 
+	number of lives, player number and other player related info.
+	Attack scripts will be a child class of this class. See LineAttacker, AreaAttacker
+*/
+public class Player : NetworkBehaviour {
+
+	protected int lives;
+	public Rigidbody rigid;
+	NetworkTransform netTransform;
+
+	float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+		rigid = this.GetComponent<Rigidbody> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isLocalPlayer)
+			return;
+
+		transform.Translate (new Vector3 (Input.GetAxis ("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxis ("Vertical") * Time.deltaTime * speed));
+
+		
+	
+	}
+}
